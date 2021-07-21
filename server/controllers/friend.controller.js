bf068ac6e855d871a8292842b467b0aed736c010
Otávio Paganotti <pganottiotavio@gmail.com>
2021-07-20 20:26:51 -0400

    add: fazendo projeto funcionar

--- server/controllers/friend.controller.js
+++ server/controllers/friend.controller.js
@@ -1,13 +1,30 @@
 import httpStatus from 'http-status';
 import db from '../models';
 import APIError from '../helpers/APIError';
+
 const { Friend, sequelize } = db;
-const load = async (req, res, next, id) => {}
-const get = async (req, res, next, id) => {}
-const create = async (req, res, next, id) => {}
-const update = async (req, res, next, id) => {}
-const list = async (req, res, next, id) => {
-    return res.json({})
-}
-const remove = async (req, res, next, id) => {}
-export default {load,get,create,update,list,remove};
+
+const load = async (req, res, next, id) => {};
+
+const get = async (req, res, next) => {};
+
+const create = async (req, res, next) => {};
+
+const update = async (req, res, next) => {};
+
+const list = async (req, res, next) => {
+  return Friend.findAll()
+    .then((friends) => res.json({friends}))
+    .catch((error) => res.boom.badRequest(error))
+};
+
+const remove = async (req, res, next) => {};
+
+export default {
+    load,
+    get,
+    create,
+    update,
+    list,
+    remove,
+};
--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -2,5 +2,7 @@ import express from 'express';
 import friendRoutes from './friend.route';
 
 const router = express.Router(); // eslint-disable-line new-cap
+
 router.use('/friends', friendRoutes);
+
 export default router;
